b9202502cf5cce5f65fbd856ad9450c625d12153
nrj022 <0602smj@gmail.com>
2024-08-14 07:50:54 +0900

    ui: 따라하기 dialog UI 레이아웃 구성

---
+++ app/src/main/java/com/example/com_us/ui/question/QuestionFollowAlongDialog.kt
@@ -0,0 +1,12 @@
+package com.example.com_us.ui.question
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.com_us.R
+
+class QuestionFollowAlongDialog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_follow_along_dialog)
+    }
+}
