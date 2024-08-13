8219a4dbc9181f8a34959f7fce60cdc561e24439
nrj022 <0602smj@gmail.com>
2024-08-14 08:30:44 +0900

    ui: 블록 획득 페이지 UI 레이아웃 구성

---
+++ app/src/main/java/com/example/com_us/ui/question/QuestionCollectBlock.kt
@@ -0,0 +1,12 @@
+package com.example.com_us.ui.question
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.com_us.R
+
+class QuestionCollectBlock : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_collect_block)
+    }
+}
