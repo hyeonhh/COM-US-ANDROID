b126eb44fff0409c52491217d430f2178cd962be
nrj022 <0602smj@gmail.com>
2024-08-15 05:44:09 +0900

    feat: 질문 리스트 테마 클릭 효과 구현

--- app/src/main/java/com/example/com_us/ui/question/QuestionFragment.kt
+++ app/src/main/java/com/example/com_us/ui/question/QuestionFragment.kt
@@ -4,18 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.com_us.R
 import com.example.com_us.databinding.FragmentQuestionBinding
 
-class QuestionFragment : Fragment() {
+class QuestionFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentQuestionBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var viewModel: QuestionViewModel
+
+    private var lastSelectedView: TextView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,11 +36,59 @@ class QuestionFragment : Fragment() {
         _binding = FragmentQuestionBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
+        /*val textView: TextView = binding.textDashboard
         questionViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
-        }
+        }*/
+
+        viewModel = ViewModelProvider(this)[QuestionViewModel::class.java]
+
+        viewModel.selectedThemeId.observe(viewLifecycleOwner, Observer {
+            val selectedView = binding.root.findViewById<TextView>(it)
+            val themeKor = selectedView.text
+            setThemeSelected(selectedView, themeKor.toString())
+        })
+
+        binding.includeThemeAll.textviewTheme.setOnClickListener(this)
+        binding.includeThemeDaily.textviewTheme.setOnClickListener(this)
+        binding.includeThemeFamily.textviewTheme.setOnClickListener(this)
+        binding.includeThemeFriend.textviewTheme.setOnClickListener(this)
+        binding.includeThemeRandom.textviewTheme.setOnClickListener(this)
+        binding.includeThemeInterest.textviewTheme.setOnClickListener(this)
+        binding.includeThemeSchool.textviewTheme.setOnClickListener(this)
+
         return root
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val themeAllView = binding.includeThemeAll
+        viewModel.updateSelectedThemeId(themeAllView.textviewTheme.id)
+    }
+
+    private fun setThemeSelected(selectedView: TextView, themeKor: String) {
+        binding.textviewQuestionTitle.text = String.format(resources.getString(R.string.question_title), themeKor)
+
+        lastSelectedView?.let { view ->
+            view.setBackgroundResource(R.drawable.shape_stroke_rect5_gray200)  // 기본 배경 리소스로 변경
+            view.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray_500))  // 기본 텍스트 색상으로 변경
+        }
+
+        selectedView?.let { view ->
+            view.setBackgroundResource(R.drawable.shape_fill_rect5_gray700)
+            view.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))  // 안전한 색상 적용
+        }
+
+        lastSelectedView = selectedView
+    }
+
+    private fun setThemeUnSelected() {
+
+    }
+
+    override fun onClick(view: View) {
+        viewModel.updateSelectedThemeId(view.id)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/com_us/ui/question/QuestionViewModel.kt
+++ app/src/main/java/com/example/com_us/ui/question/QuestionViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.com_us.ui.question
 
+import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.com_us.R
 
 class QuestionViewModel : ViewModel() {
 
@@ -10,4 +12,13 @@ class QuestionViewModel : ViewModel() {
         value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
+
+    private val _selectedThemeId = MutableLiveData<Int>().apply {
+        value = R.id.include_theme_all
+    }
+    val selectedThemeId: LiveData<Int> = _selectedThemeId
+
+    fun updateSelectedThemeId(newId: Int) {
+        _selectedThemeId.value = newId
+    }
 }
