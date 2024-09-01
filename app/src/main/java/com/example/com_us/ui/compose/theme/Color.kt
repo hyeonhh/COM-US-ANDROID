b28427a8bc6c775de7e2d2cf7adb47dde7aebb8b
nrj022 <0602smj@gmail.com>
2024-09-01 13:14:33 +0900

    add: compose gray 컬러 종류 추가

--- app/src/main/java/com/example/com_us/ui/compose/theme/Color.kt
+++ app/src/main/java/com/example/com_us/ui/compose/theme/Color.kt
@@ -12,7 +12,9 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val Gray100 = Color(0xFFD5D5D5)
 val Gray200 = Color(0xFFD8D8D8)
+val Gray400 = Color(0xFF878787)
 val Gray500 = Color(0xFF8F8F8F)
 
 val Blue700 = Color(0xFF00A3E8)
