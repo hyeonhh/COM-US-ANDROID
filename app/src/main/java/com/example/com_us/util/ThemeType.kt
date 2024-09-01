3bf4ad5ee8975edcbb545cc53384bd1ae8e5c59b
nrj022 <0602smj@gmail.com>
2024-09-01 13:09:31 +0900

    add: 테마와 블럭 이미지 매칭

--- app/src/main/java/com/example/com_us/util/ThemeType.kt
+++ app/src/main/java/com/example/com_us/util/ThemeType.kt
@@ -1,18 +1,25 @@
 package com.example.com_us.util
 
-enum class ThemeType(val kor: String) {
-    ALL("전체"),
-    DAILY("일상"),
-    SCHOOL("학교"),
-    FRIEND("친구"),
-    FAMILY("가족"),
-    HOBBY("관심사"),
-    RANDOM("랜덤");
+import androidx.compose.ui.res.stringResource
+import com.example.com_us.R
+
+enum class ThemeType(val kor: String, val blockRes: Int?) {
+    ALL("전체", null),
+    DAILY("일상", R.drawable.img_block_char_orange),
+    SCHOOL("학교", R.drawable.img_block_char_blue),
+    FRIEND("친구", R.drawable.img_block_char_purple),
+    FAMILY("가족", R.drawable.img_block_char_salmon),
+    HOBBY("관심사", R.drawable.img_block_char_green),
+    RANDOM("랜덤", null);
 
     companion object {
         // kor 값으로 ThemeType을 찾는 메서드
         fun fromKor(kor: String): ThemeType? {
             return values().find { it.kor == kor }
+        }
+
+        fun findResFromKor(kor: String): Int? {
+            return values().find { it.kor == kor }?.blockRes
         }
     }
 }
