5f37268fcb687fde0cc246b5a3299c754e119515
nrj022 <0602smj@gmail.com>
2024-09-01 13:26:20 +0900

    feat: 답변 결과 상세보기 페이지 추가

---
+++ app/src/main/java/com/example/com_us/ui/question/QuestionResultActivity.kt
@@ -0,0 +1,12 @@
+package com.example.com_us.ui.question
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.com_us.R
+
+class QuestionResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_result)
+    }
+}
